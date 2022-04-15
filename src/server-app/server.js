f2d76e9bcb43298f7a4558d92b819643b0c066f7
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-04-16 01:46:51 +0530

    Configure sending of assets

--- src/server-app/server.js
+++ src/server-app/server.js
@@ -10,6 +10,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve("build/index.html"));
 });
 
+app.get("/static/:folder/:file", (req, res) => {
+  res.sendFile(
+    path.resolve(`build/static/${req.params.folder}/${req.params.file}`)
+  );
+}); // for JS, CSS and other React app assets
+
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`);
 });
