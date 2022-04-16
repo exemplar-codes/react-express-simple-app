9ceac33bc5dff822ce8663132c544dcc49b799df
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-04-16 10:57:52 +0530

    Correct devStart and production port

--- src/server-app/server.js
+++ src/server-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = (process.env.PORT && 80) || 3000;
+const port = process.env.PORT || 80; // Heroku, or local or production
 
 const path = require("path");
 
